57c1cc605e2b5d6c195c6baaa16a523144591f5c
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-11-02 17:25:18 +0100

    web_dynamic/1-hbnb.py

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,1 +1,13 @@
-console.log("hello from 1-hbnb.js");
+// Script that listens for changes on each INPUT checkbox tag
+$(document).ready(() => {
+  const amenities = {};
+  $('input[type="checkbox"]').change(() => {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    const amenitiesList = Object.values(amenities).join(', ');
+    $('.amenities h4').text(amenitiesList);
+  });
+});
