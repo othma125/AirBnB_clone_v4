da031aacd0f432b39ad9264681846081283c640d
bensbahou <younesbensbahou@gmail.com>
2023-11-06 00:39:59 +0100

    1

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,9 +2,9 @@
 $(document).ready(() => {
   let clicked = [];
 
-  $(':checkbox').on('click', function () {
+  $(":checkbox").on("click", function () {
     // Use 'function()' instead of '() =>' to properly bind 'this'
-    const currentId = $(this).data('id'); // Correctly retrieve 'data-id' attribute
+    const currentId = $(this).data("id"); // Correctly retrieve 'data-id' attribute
 
     if (clicked.indexOf(currentId) === -1) {
       clicked.push(currentId);
@@ -14,16 +14,16 @@ $(document).ready(() => {
 
     // Construct the list of names based on the 'clicked' array
     const text = [];
-    $('.popover li input').each(function () {
-      if (clicked.indexOf($(this).data('id')) !== -1) {
-        text.push($(this).data('name'));
+    $(".popover li input").each(function () {
+      if (clicked.indexOf($(this).data("id")) !== -1) {
+        text.push($(this).data("name"));
       }
     });
 
     if (text.length > 0) {
-      $('.amenities h4').text(text.join(', '));
+      $(".amenities h4").text(text.join(", "));
     } else {
-      $('.amenities h4').html('&nbsp;');
+      $(".amenities h4").html("&nbsp;");
     }
   });
 });
