58d698ac8a50e2d0c13cd53476b39ecfa7b613bf
bensbahou <younesbensbahou@gmail.com>
2023-11-06 00:40:11 +0100

    2

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,17 +1,17 @@
 /* global $ */
 $(document).ready(() => {
-  $.get('http://localhost:5001/api/v1/status/', (data) => {
+  $.get("http://localhost:5001/api/v1/status/", (data) => {
     // If the status is "OK", add the "available" class to the div#api_status
-    if (data.status === 'OK') {
-      $('div#api_status').addClass('available');
+    if (data.status === "OK") {
+      $("div#api_status").addClass("available");
     } else {
       // Otherwise, remove the "available" class from the div#api_status
-      $('div#api_status').removeClass('available');
+      $("div#api_status").removeClass("available");
     }
   });
   let clicked = [];
-  $(':checkbox').on('click', function () {
-    const currentId = $(this).data('id'); // Correctly retrieve 'data-id' attribute
+  $(":checkbox").on("click", function () {
+    const currentId = $(this).data("id"); // Correctly retrieve 'data-id' attribute
     if (clicked.indexOf(currentId) === -1) {
       clicked.push(currentId);
     } else {
@@ -19,15 +19,15 @@ $(document).ready(() => {
     }
     // Construct the list of names based on the 'clicked' array
     const text = [];
-    $('.popover li input').each(function () {
-      if (clicked.indexOf($(this).data('id')) !== -1) {
-        text.push($(this).data('name'));
+    $(".popover li input").each(function () {
+      if (clicked.indexOf($(this).data("id")) !== -1) {
+        text.push($(this).data("name"));
       }
     });
     if (text.length > 0) {
-      $('.amenities h4').text(text.join(', '));
+      $(".amenities h4").text(text.join(", "));
     } else {
-      $('.amenities h4').html('&nbsp;');
+      $(".amenities h4").html("&nbsp;");
     }
   });
 });
